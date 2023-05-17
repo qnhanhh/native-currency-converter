5003401360ebe0ab97627687409633d2b750b9e7
qnhanhh <caoquynhanh18@gmail.com>
2023-05-17 17:55:29 +0700

    add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['handlebarlabs'],
+    rules: {
+        'react/jsx-props-no-spreading': 0,
+        'react/jsx-curly-newline': 0,
+        'react/style-prop-object': 0,
+        'react/react-in-jsx-scope': false
+    }
+}
