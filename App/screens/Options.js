9d337eb7b9ab3acdf93d9936dd52c0eb58896130
qnhanhh <caoquynhanh18@gmail.com>
2023-05-22 15:55:02 +0700

    open links from app

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,35 +1,42 @@
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, ScrollView, Linking, Alert } from 'react-native'
 import { Entypo } from '@expo/vector-icons'
 import colors from '../constants/colors'
 import { RowItem, RowSeparator } from '../components/RowItem'
 
+const openUrl = (url) => {
+    return Linking.openURL(url).catch(() => {
+        Alert.alert('Sorry, something went wrong.', 'Please try again later.')
+    })
+}
 export default function Options() {
     return (
-        <SafeAreaView>
-            <RowItem
-                text="Themes"
-                onPress={() => alert("press")}
-                rightIcon={
-                    <Entypo name='chevron-right' size={20} color={colors.blue} />
-                } />
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView>
+                <RowItem
+                    text="Themes"
+                    onPress={() => alert("press")}
+                    rightIcon={
+                        <Entypo name='chevron-right' size={20} color={colors.blue} />
+                    } />
 
-            <RowSeparator />
+                <RowSeparator />
 
-            <RowItem
-                text="React Native Basics"
-                onPress={() => alert("press")}
-                rightIcon={
-                    <Entypo name='export' size={20} color={colors.blue} />
-                } />
+                <RowItem
+                    text="React Native Basics"
+                    onPress={() => openUrl('https://learn.handlebarlabs.com/p/react-native-basics-build-a-currency-converter')}
+                    rightIcon={
+                        <Entypo name='export' size={20} color={colors.blue} />
+                    } />
 
-            <RowSeparator />
+                <RowSeparator />
 
-            <RowItem
-                text="React Native by Example"
-                onPress={() => alert("press")}
-                rightIcon={
-                    <Entypo name='export' size={20} color={colors.blue} />
-                } />
+                <RowItem
+                    text="React Native by Example"
+                    onPress={() => openUrl('httpsxsdasd://learn.handlebarlabs.com/p/react-native-basics-build-a-currency-converter')}
+                    rightIcon={
+                        <Entypo name='export' size={20} color={colors.blue} />
+                    } />
+            </ScrollView>
         </SafeAreaView>
     )
 }
