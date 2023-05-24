c57a387774d753db8787dce16dcd2c96126790fc
qnhanhh <caoquynhanh18@gmail.com>
2023-05-24 11:05:51 +0700

    add images

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,10 +1,26 @@
-import { View, StyleSheet, StatusBar } from 'react-native'
+import { View, StyleSheet, StatusBar, Image, Dimensions } from 'react-native'
 import colors from '../constants/colors'
+
+const screen = Dimensions.get('window')
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.blue,
-        flex: 1
+        flex: 1,
+        justifyContent: 'center'
+    },
+    logoContainer: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logoBackground: {
+        width: screen.width * 0.45,
+        height: screen.width * 0.45
+    },
+    logo: {
+        position: 'absolute',
+        width: screen.width * 0.25,
+        height: screen.width * 0.25
     }
 })
 
@@ -12,6 +28,18 @@ export default function Home() {
     return (
         <View style={styles.container}>
             <StatusBar barStyle='light-content' backgroundColor={colors.blue} />
+            <View style={styles.logoContainer}>
+                <Image
+                    style={styles.logoBackground}
+                    source={require('../assets/images/background.png')}
+                    resizeMode='contain'
+                />
+                <Image
+                    style={styles.logo}
+                    source={require('../assets/images/logo.png')}
+                    resizeMode='contain'
+                />
+            </View>
         </View>
     )
 }
