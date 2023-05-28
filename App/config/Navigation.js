00ade63e79f915471128bac7a974290dbb8b7ebe
qnhanhh <caoquynhanh18@gmail.com>
2023-05-28 23:02:31 +0700

    cross icon for modal

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,8 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { TouchableOpacity } from "react-native";
+import { Entypo } from '@expo/vector-icons'
 import Home from '../screens/Home'
 import Options from '../screens/Options'
 import CurrencyList from '../screens/CurrencyList'
+import colors from "../constants/colors";
 
 const MainStack = createStackNavigator()
 const MainStackScreen = () => {
@@ -19,8 +22,16 @@ const ModalStackScreen = () => {
     return (
         <ModalStack.Navigator screenOptions={{ presentation: 'modal' }}>
             <ModalStack.Screen name="Main" component={MainStackScreen} options={{ headerShown: false }} />
-            <ModalStack.Screen name="CurrencyList" component={CurrencyList} options={({ route }) => {
-                return { title: route.params && route.params.title }
+            <ModalStack.Screen name="CurrencyList" component={CurrencyList} options={({ route, navigation }) => {
+                return {
+                    title: route.params && route.params.title,
+                    headerLeft: null,
+                    headerRight: () => (
+                        <TouchableOpacity onPress={() => navigation.pop()} style={{ paddingHorizontal: 10 }}>
+                            <Entypo name='cross' size={30} color={colors.blue} />
+                        </TouchableOpacity>
+                    )
+                }
             }} />
         </ModalStack.Navigator>
     )
