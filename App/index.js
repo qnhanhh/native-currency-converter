25b938fc1b75351b3ab8c93b8f34a813607ab3f6
qnhanhh <caoquynhanh18@gmail.com>
2023-05-29 02:29:21 +0700

    simple local api

--- App/index.js
+++ App/index.js
@@ -1,4 +1,9 @@
 import Navigation from "./config/Navigation";
+import { api } from './util/api'
+
+api('/latest?base=USD')
+  .then(res => console.log('response', res))
+  .catch(err => console.log('error', err))
 
 export default function Index() {
   return (
